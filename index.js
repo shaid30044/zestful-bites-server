2b85b21342d22a7f20fc2140e81e162dc86ce4b2
shaid30044 <kaona30044@gmail.com>
2023-11-22 15:48:35 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,44 @@
+require("dotenv").config();
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.2mr4msx.mongodb.net/?retryWrites=true&w=majority`;
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    const itemCollection = client.db("Zestful_Bites_DB").collection("items");
+
+    app.get("/items", async (req, res) => {
+      const result = await itemCollection.find().toArray();
+      res.send(result);
+    });
+
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+  }
+}
+run().catch(console.dir);
+
+app.get("/", (req, res) => {
+  res.send("Zestful Bites is running.");
+});
+
+app.listen(port, () => {
+  console.log(`Zestful Bites is running on port ${port}`);
+});
